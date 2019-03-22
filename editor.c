9bd74b56a71785fa2d5aeb25d206267c39dfe93f
glufke <glufke@Glufke-MacBook-Pro.local>
2019-03-22 15:23:11 -0700

    Removed crappy TAB chars

--- editor.c
+++ editor.c
@@ -22,107 +22,98 @@ int mapa[16][16];
 //created this pattern for test for now
 static const unsigned int cross_pattern[] = {
   0b1111111111111111,
-  0b1001001110010011,
-  0b1001000110010011,
-  0b1111111111111111,
-  0b1001000110010011,
-  0b1001000110010011,
-  0b1001001110010011,
-  0b1111111111111111,
-  0b1001001110010011,
-  0b1001000110010011,
-  0b1111111111111111,
-  0b1001000110010011,
-  0b1001000110010011,
-  0b1001001110010011,
-  0b1001001110010011,  
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,
+  0b0000000000000000,  
   0b1111111111111111
 };
 
-//
+// --------------------------------------------------------------------
 // For now, this is a table 8x8 (TODO: to be implemented later)
-//
+// --------------------------------------------------------------------
 void tabela16(int x1, int y1)
 {
-	int j;
-
-	for (j=0; j<16; j++) {
-		Locate(x1,y1+j);
-		Print("................");
-	}
+  int j;
+    for (j=0; j<16; j++) {
+    Locate(x1,y1+j);
+    Print("................");
+  }
 }
 
-//
+// --------------------------------------------------------------------
 // Update sprite in real time
-//
+// --------------------------------------------------------------------
 void updateSprite(void)
 {
-	PutSprite( 0, 0, 20, 20,  (0 & 7) + 8 );
-	
+  PutSprite( 0, 0, 20, 20,  (0 & 7) + 8 );
+  
 }
  
-
-
-//
+// --------------------------------------------------------------------
 // Main program.
-//
+// --------------------------------------------------------------------
 void laco()
 {
-	int c;
+  int c;
+  do 
+  {   
+    c=WaitKey();  
+    //for now, showing the ascii cursor in the 0,0 (remove)
+    Locate(0,0);
+    PrintNumber(c);
 
-	do 
-	{   
-		
-		c=WaitKey();  
-		//for now, showing the ascii cursor in the 0,0 (remove)
-		Locate(0,0);
-		PrintNumber(c);
-
-	    if      (c==AESQ) { if (x==0)  {x=16;}  x=x-1;}
-		else if (c==ADIR) { if (x==15) {x=-1;}  x=x+1;}
-        else if (c==ACIM) { if (y==0)  {y=16;}  y=y-1;}
-		else if (c==ABAI) { if (y==15) {y=-1;}  y=y+1;}
-		else if (c==AESP) { if (mapa[x][y]==0) 
-			                {mapa[x][y]=1;
-		                    }
-		                    else 
-		                    {mapa[x][y]=0; 
-		                    }
-		                  }
-
-        Locate(x+BOXX,y+BOXY);
-        if (c==AESP) 
-        {
-            if ( mapa[x][y] ==0 ) Print("X"); else Print(".");
-        	Locate(x+BOXX,y+BOXY);
-        	updateSprite();
-        }           
-
-	} while (c != AESC ); 
-   
+    if      (c==AESQ) { if (x==0)  {x=16;}  x=x-1;}
+    else if (c==ADIR) { if (x==15) {x=-1;}  x=x+1;}
+    else if (c==ACIM) { if (y==0)  {y=16;}  y=y-1;}
+    else if (c==ABAI) { if (y==15) {y=-1;}  y=y+1;}
+    else if (c==AESP) { if (mapa[x][y]==0) 
+                        {mapa[x][y]=1;
+                        }
+                        else 
+                        {mapa[x][y]=0; 
+                        }
+                      }
+    Locate(x+BOXX,y+BOXY);
+    if (c==AESP) 
+    {
+      if ( mapa[x][y] ==0 ) Print("X"); else Print(".");
+      Locate(x+BOXX,y+BOXY);
+      updateSprite();
+    }           
+  } while (c != AESC ); 
 }
 
-
-
-
+// --------------------------------------------------------------------
+// M A I N 
+// --------------------------------------------------------------------
 void main(void)
 {
-	Screen(1);
- 	Cls();
+  Screen(1);
+  Cls();
 
- 	//set Sprite table
-	SpriteReset();
-	Sprite16();
-	SpriteSmall();
-	SetSpritePattern( 0, Sprite32Bytes(cross_pattern), 256 );
+  //set Sprite table
+  SpriteReset();
+  Sprite16();
+  SpriteSmall();
+  SetSpritePattern( 0, Sprite32Bytes(cross_pattern), 256 );
 
+  x=0;
+  y=0;
+  tabela16(BOXX,BOXY);
+  Locate(x+BOXX,y+BOXY);
+  laco();
 
- 	x=0;
-  	y=0;
- 	tabela16(BOXX,BOXY);
-    Locate(x+BOXX,y+BOXY);
-    laco();
-
- 	Exit(0);
+  Exit(0);
 }
 
